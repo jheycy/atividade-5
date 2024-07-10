237b3336658c4fc640feecdb54b4cd0fe5b1186f
jheycy <paulojheycy@gmail.com>
2024-07-10 16:49:17 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,70 @@
+<?php
+// q 1//
+
+echo "Quero casa" . "<br>";
+
+
+//q2//
+
+$nome= 'jheycy';
+$idade = '19 anos';
+$endereço= 'em São Luis';
+$curso= 'programador web no SENAC';
+
+
+echo "Ola eu sou  " . $nome . "<br>";
+echo "tenho " .$idade . "<br>";
+echo "moro " . $endereço . "<br>";
+echo " faço o curso " . $curso . "<br>";
+
+//q3//
+
+$numero1 = (10);
+$numero2 = (2);
+
+$soma = $numero1 + $numero2;
+$subtracao = $numero1 - $numero2;
+$multiplicacao = $numero1 * $numero2;
+$divisao = $numero1 / $numero2;
+
+echo "Soma: " . $soma . "<br>";
+echo "Subtração: " . $subtracao . "<br>";
+echo "Multiplicação: " . $multiplicacao . "<br>";
+if ($numero2 != 0) {
+    echo "Divisão: " . $divisao . "<br>";
+} else {
+    echo "Divisão: Não é possível dividir por zero\n";
+}
+ echo "<br>";
+
+
+ //Q4//
+
+ $num1 = (10);
+$num2 = (2);
+
+?>
+ 
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" 
+    content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> </h2>
+    
+</body>
+</html>
